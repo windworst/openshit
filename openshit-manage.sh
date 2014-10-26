4f1df76a4a17bdbc4c4953b167d72f5f20c27289
Demon_pace <windworst@gmail.com>
2014-10-26 12:55:24 +0800

    keystone configure

--- openshit-manage.sh
+++ openshit-manage.sh
@@ -2,6 +2,8 @@
 SCRIPT_NAME=openshit-manage.sh
 CONFIG_FILE=openshit.conf
 SERVICE_PATH=service
+SERVICE_ENV_FILE=service-env.sh
+ADMIN_ENV_FILE=admin-env.sh
 
 do_export()
 {
@@ -10,8 +12,32 @@ do_export()
     exit 1
   fi
 
-  echo "Loading..configuration"
   source $CONFIG_FILE
+}
+
+# args: dbname password
+set_database()
+{
+  DBNAME=$1
+  DBPASS=$2
+  echo "Create Database ${DBNAME}"
+  echo "DROP DATABASE IF EXISTS ${DBNAME};\
+    CREATE DATABASE ${DBNAME};\
+    GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${DBNAME}'@'localhost' \
+    IDENTIFIED BY '${DBPASS}';
+  GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${DBNAME}'@'%' \
+    IDENTIFIED BY '${DBPASS}';" \
+    | mysql -u${SET_MYSQL_USER} -p${SET_MYSQL_PASS}
+}
+
+# args: key value file
+set_conf_arg()
+{
+  KEY=$1
+  VALUE=$2
+  FILE=$3
+  echo "${FILE}: ${KEY}${VALUE}"
+  sudo sed -i "s/^[#, ]*${KEY}.*/${KEY}${VALUE}/g" ${FILE}
 }
 
 help()
@@ -25,9 +51,9 @@ help()
 
 if [ $# -le 0 ]; then
   help
-elif [ -e "$SERVICE_PATH/"$1 ]; then
+elif [ -e "${SERVICE_PATH}/"$1 ]; then
   do_export
-  source "$SERVICE_PATH/"$1 $@
+  source "${SERVICE_PATH}/"$1 $@
 else
   help
 fi
